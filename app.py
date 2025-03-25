b96b713d54e7e999dedfae0f9ce3738302397748
sezyu <129446663+sezyu@users.noreply.github.com>
2025-03-25 19:16:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 def my_function():
     print('Hello from my_function!')
+    print('Hello from my_function!')
+    print('Hello from my_function!')
+
+
 if __name__ == '__main__':
     my_function()
